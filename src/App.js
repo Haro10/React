edd417eceb3f3cb526ee2fbc222dd1bfdf61c08e
Ha Kieu Minh <minhha19122014@gmail.com>
2019-02-10 17:58:38 +0900

    add toggle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from '../../../../../../../../../../Downloads/base-syntax--02-state-events/src/Person/Person';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,12 +35,28 @@ class App extends Component {
     } )
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render () {
+const style = {
+  backgroundColor: 'white',
+  font: 'inherit',
+  border: '1px solid blue',
+  padding: '10px',
+  cursor: 'pointer'
+}
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <button style={style}
+        onClick={() => this.togglePersonHandler}>Switch Name</button>
+     { this.state.showPersons ? 
+       <div>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
@@ -51,6 +68,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
+      </div> : null
+     } 
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
