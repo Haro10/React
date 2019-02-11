396e17cba463335458a8ffcf76b535231d3902f0
Ha Kieu Minh <minhha19122014@gmail.com>
2019-02-11 11:45:24 +0900

    Flexible list

--- src/App.js
+++ src/App.js
@@ -15,13 +15,25 @@ class App extends Component {
 
 
 
-  nameChangedHandler = ( event ) => {
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+   const person = {
+     ...this.state.persons[personIndex]
+   };
+   
+   person.name = event.target.value;
+   
+   // const person = Object.assign({}, this.state.persons[personIndex]);
+
+   const persons = [...this.state.persons];
+
+   persons[personIndex] = person;
+
     this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+      persons: persons
     } )
   }
 
@@ -56,16 +68,27 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map((person,index) => {
-            return <Person 
-             click = {
-               () => this.deletePersonHandler(index)
-             }
-            name={person.name}
-            age={person.age} 
-            key={person.id}
-            />
-          })}
+           {
+             this.state.persons.map((person, index) => {
+               return <Person
+               click = {
+                 () => this.deletePersonHandler(index)
+               }
+               name = {
+                 person.name
+               }
+               age = {
+                 person.age
+               }
+               key = {
+                 person.id
+               }
+               changed = {
+                 (event) => this.nameChangedHandler(event, person.id)
+               }
+               />
+             })
+           }
         </div>
       );
     }
